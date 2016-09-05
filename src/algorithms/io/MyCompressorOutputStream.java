2d8803712806a9c19d8a95039ea34b8c22152596
Adi-Haviv <adi.haviv@gmail.com>
2016-09-05 19:22:36 +0300

    דעעעע

--- src/algorithms/io/MyCompressorOutputStream.java
+++ src/algorithms/io/MyCompressorOutputStream.java
@@ -1,14 +1,5 @@
 package algorithms.io;
 
-import java.io.IOException;
-import java.io.OutputStream;
-
 public class MyCompressorOutputStream extends OutputStream {
-
-	@Override
-	public void write(int arg0) throws IOException {
-		// TODO Auto-generated method stub
-
-	}
-
+	
 }
