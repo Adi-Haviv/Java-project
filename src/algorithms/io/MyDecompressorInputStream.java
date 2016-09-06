030d197f9fd2aa99e2f01499a712502b0ddfef27
Adi-Haviv <adi.haviv@gmail.com>
2016-09-06 17:32:47 +0300

    added input stream

--- src/algorithms/io/MyDecompressorInputStream.java
+++ src/algorithms/io/MyDecompressorInputStream.java
@@ -4,11 +4,41 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class MyDecompressorInputStream extends InputStream{
+	InputStream in;
+	
+	public MyDecompressorInputStream(InputStream in) {
+		this.in = in;
+	}
 
 	@Override
 	public int read() throws IOException {
-		// TODO Auto-generated method stub
-		return 0;
+		try{
+			return in.read();
+		}
+		catch (Exception IOException) {
+			return -1;
+		}
 	}
+	
+	public int read(byte[] b) throws IOException{
+		byte reps = 0;
+		byte value;
+		int counter = 0;
+		if(b.length == 0){
+			return 0;
+		}
+		
+		while(counter < b.length){
+			reps = (byte) (reps + in.read());
+			value = (byte) in.read();
+			
+			while(counter < reps){
+				b[counter] = (byte) value;
+			}
+		}
+		
+		return counter;
+	}
+	
 
 }
