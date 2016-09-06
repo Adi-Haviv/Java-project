f338f4bc35760d03d158132d9e02023b8386484c
Adi-Haviv <adi.haviv@gmail.com>
2016-09-06 18:58:27 +0300

    updated input stream

--- src/algorithms/io/MyDecompressorInputStream.java
+++ src/algorithms/io/MyDecompressorInputStream.java
@@ -21,19 +21,26 @@ public class MyDecompressorInputStream extends InputStream{
 	}
 	
 	public int read(byte[] b) throws IOException{
+		// reps receives repetitions of specific value.
+		// counter is used to count bytes already read, as well as control the loops.
 		byte reps = 0;
 		byte value;
 		int counter = 0;
+		
+		// If the receiving array is not defined, return 0 meaning 0 bytes read.
 		if(b.length == 0){
 			return 0;
 		}
 		
+		// While b is not full and stream still has data, decompress data from stream and place in b 
 		while(counter < b.length){
 			reps = (byte) (reps + in.read());
+			
 			value = (byte) in.read();
 			
 			while(counter < reps){
 				b[counter] = (byte) value;
+				counter++;
 			}
 		}
 		
