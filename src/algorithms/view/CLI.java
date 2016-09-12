5218b3d34688caf0b87c2c917e45198ec810c7fc
Adi-Haviv <adi.haviv@gmail.com>
2016-09-12 15:49:29 +0300

    s

--- src/algorithms/view/CLI.java
+++ src/algorithms/view/CLI.java
@@ -3,14 +3,21 @@ package algorithms.view;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
-
 import algorithms.controller.Command;
 
+/**
+ * <h1> CLI Class </h1>
+ * This class is a basic CLI - Command Line Interface.
+ * Receives commands by input from a BufferedReader and capable of writing to a PrintWriter.
+ * Commands are held in a HashMap<String,Command> object.
+ * 
+ * @author Adi Haviv & Bar Genish
+ *
+ */
 public class CLI {
 	BufferedReader in;
 	PrintWriter out;
@@ -18,45 +25,30 @@ public class CLI {
 	final Queue<String> userCommands = new LinkedList<String>();
 	String command;
 	
-	// IOLoop class implements callable to be called as a separate thread, and return value to main
-	public class IOLoop implements Runnable{
-		ArrayList<String> commands = new ArrayList<String>();
-		String command;
-		BufferedReader in;
-		
-		// C'Tor for input stream
-		IOLoop(BufferedReader in){
-			this.in = in;
-		}
-		
-		// Runs when started as a thread
-		@Override
-		public void run() {
-			try{
-				// Creates arraylist of string to return from user input to main thread.
-				command = in.readLine();
-				while(!command.equals(new String("exit"))){
-					Thread.sleep(100);
-					userCommands.add(command);
-					command = in.readLine();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}			
-		}	
-	}
-	
+	/**
+	 * C'Tor
+	 * @param in BufferedReader to be used for input.
+	 * @param out PrintWriter to be used for output.
+	 */
 	CLI(BufferedReader in, PrintWriter out){
 		this.in = in;
 		this.out = out;
 	}
 
+	/**
+	 * Sets the commands data member to commands hashmap received as parameter.
+	 * @param commands HashMap that maps Command objects to user String objects.
+	 */
 	public void setCommands(HashMap<String, Command> commands){
 		this.commands = commands;
 	}
 	
+	/**
+	 * The main method for the CLI class.
+	 * Starts the IOLoop in charge of user input in a seperate thread.
+	 * The loop then inputs command lines from user, fetches respective Command object from HashMap,
+	 * and calls the Command's doCommand method.
+	 */
 	public void start(){
 		Thread IOThread = new Thread(new Runnable() {
 			Command current;
@@ -92,6 +84,11 @@ public class CLI {
 		});
 		IOThread.start();
 	}	
+	
+	/**
+	 * Prints string to PrintWriter data member of CLI.
+	 * @param string String to be written to PrintWriter.
+	 */
 	public void write(String string){
 		this.out.write(string);	
 	}
--- src/algorithms/view/MyView.java
+++ src/algorithms/view/MyView.java
@@ -10,6 +10,14 @@ import algorithms.mazeGenerators.Maze3d;
 import algorithms.mazeGenerators.Position;
 import algorithms.search.Solution;
 
+/**
+ * <h1> MyView Class </h1>
+ * This Class implements the View interface and manages all interactions with the user.
+ * The Class manages the CLI object in charge of user input and running commands.
+ * 
+ * @author Adi Haviv & Bar Genish
+ *
+ */
 public class MyView implements View {
 	HashMap<String, Command> commands;
 	CLI cli;
@@ -17,6 +25,11 @@ public class MyView implements View {
 	BufferedReader in;
 	Controller controller;
 	
+	/**
+	 * C'Tor
+	 * @param in BufferedReader to be used for input.
+	 * @param out PrintWriter to be used for output.
+	 */
 	MyView(BufferedReader in, PrintWriter out){
 		this.in = in;
 		this.out = out;
@@ -24,14 +37,24 @@ public class MyView implements View {
 		cli = new CLI(in,out);
 	}
 	
+	/**
+	 * Sets MyView's controller.
+	 * @param controller
+	 */
 	public void setController(Controller controller){
 		this.controller = controller;
 	}
 	
+	/**
+	 * Starts the cli and the programs functionality.
+	 */
 	public void start(){
 		cli.start();
 	}
 	
+	/**
+	 * Notify user maze generation is complete.
+	 */
 	@Override
 	public void notifyMazeIsReady(String name) {
 		String output = name + " maze is ready, Have Fun!";
@@ -39,29 +62,45 @@ public class MyView implements View {
 
 	}
 
+	/**
+	 * Prints a maze to user output stream.
+	 */
 	@Override
 	public void displayMaze(Maze3d maze) {
 		cli.write(maze.toString());
 	}
-
+	
+	/**
+	 * Sets the commands hashmap for MyView and CLI.
+	 */
 	@Override
 	public void setCommands(HashMap<String, Command> commands) {
 		this.commands = commands;
 		cli.setCommands(commands);
 	}
 
+	/**
+	 * Displays a directory's contents.
+	 * @param dirContents
+	 */
 	@Override
 	public void displayDirectoryContents(String dirContents) {
 		cli.write(dirContents);
 		
 	}
 
+	/**
+	 * Displays a maze's cross section.
+	 */
 	@Override
 	public void displayCrossSectionBy(int[][] maze2d) {
 		cli.write(maze2d.toString());
 		
 	}
 
+	/**
+	 * Notify user maze is saved to file.
+	 */
 	@Override
 	public void notifyMazeIsSaved(String name) {
 		String output = name + " maze was saved succesfully";
@@ -69,6 +108,9 @@ public class MyView implements View {
 		
 	}
 
+	/**
+	 * Notify user maze is loaded to memory.
+	 */
 	@Override
 	public void notifyMazeIsLoaded(String name) {
 		String output = name + " maze was loaded succesfully";
@@ -76,16 +118,21 @@ public class MyView implements View {
 		
 	}
 
+	/**
+	 * Notify user a solution is available for the given maze.
+	 */
 	@Override
 	public void notifyMazeIsSolved(String name) {
 		String output = name + " maze was solved";
 		cli.write(output);		
 	}
 
+	/**
+	 * Display a maze's solution to user.
+	 */
 	@Override
 	public void displaySolution(Solution<Position> sol){
-		cli.write(sol.toString());
-		
+		cli.write(sol.toString());	
 	}
 	
 	
