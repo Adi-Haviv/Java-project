887f95d338e5489a3d37a544b71e8cb90fb6df49
Bar <Bar@Bars-pc>
2016-09-14 20:57:24 +0300

    i<3java

--- src/algorithms/view/MyView.java
+++ src/algorithms/view/MyView.java
@@ -72,7 +72,7 @@ public class MyView implements View {
 		try{
 			cli.write(maze.toString());
 		}catch (NullPointerException e) {
-			controller.write("Not a valid maze");
+			e.printStackTrace();
 		}
 	}
 	
