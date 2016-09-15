da0633633c434aea5331b681cee66e7b5b39b4df
Adi-Haviv <adi.haviv@gmail.com>
2016-09-15 18:56:58 +0300

    s

--- src/algorithms/io/MyDecompressorInputStream.java
+++ src/algorithms/io/MyDecompressorInputStream.java
@@ -4,13 +4,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+/**
+ * <h1> Decompressor Input Class </h1>
+ * This class defines an input stream that decompresses data written to a stream using MyCompressorOutputStream.
+ * Returns a byte array or a List of Byte Objects containing all the data from the InputStream. 
+ * 
+ * @author Adi Haviv & Bar Genish
+ *
+ */
 public class MyDecompressorInputStream extends InputStream{
 	InputStream in;
 	
+	/**
+	 * C'tor
+	 * @param in InputStream from which to read compressed data.
+	 */
 	public MyDecompressorInputStream(InputStream in) {
 		this.in = in;
 	}
-
+	
+	/**
+	 * Reads a single byte from in.
+	 * @return int Returns the next byte from stream as int between 0-255.
+	 */
 	@Override
 	public int read() throws IOException {
 		try{
@@ -21,6 +37,12 @@ public class MyDecompressorInputStream extends InputStream{
 		}
 	}
 	
+	/**
+	 * Reads into a byte[] object until it's full or until stream is empty.
+	 * @param b byte[] to read data into.
+	 * @return int Number of bytes read into b.
+	 * @throws IOException If input stream is not defined properly.
+	 */
 	public int read(byte[] b) throws IOException{
 		// reps receives repetitions of specific value.
 		// counter is used to count bytes already read, as well as control the loops.
@@ -47,6 +69,12 @@ public class MyDecompressorInputStream extends InputStream{
 		return b.length;
 	}
 	
+	/**
+	 * Reads into a List of Byte objects until stream is empty.
+	 * @param list List of Byte obejects to fill with data from stream.
+	 * @return int Number of bytes read from the stream.
+	 * @throws IOException When input stream isn't defined properly.
+	 */
 	public int read(List<Byte> list) throws IOException{
 		// reps receives repetitions of specific value.
 		// counter is used to count bytes already read, as well as control the loops.
